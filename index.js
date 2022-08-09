12afca5838412f60e980f2be0147a3a0b66c1b27
Jasmin <jasmin.parish@outlook.com>
2022-08-08 23:47:27 -0400

    commit tree d47e8dad35e4e933d77e2a918b872fe58f0c1d3f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+   console.log(`This Saturday, I want to ${activity}!`) 
+  return saturdayFun
+    
+};
+ saturdayFun()
+
+
+
